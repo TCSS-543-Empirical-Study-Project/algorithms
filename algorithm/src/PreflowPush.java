d6cd6756cf5075e57f2fc01774a245a44be2527c
Todd Robbins <toddrobb11@gmail.com>
2017-11-29 15:29:42 -0800

    setup for pre flow push class

---
+++ algorithm/src/PreflowPush.java
@@ -0,0 +1,24 @@
+
+/**
+ * the PreflowPush class implements the PreoflowPush algorithm
+ * 
+ * @author Todd Robbins & Brandon Lioce
+ */
+public class PreflowPush {
+
+    private SimpleGraph sGraph;
+//    private HeightList heights;
+	
+	/**
+	 * find the max-flow
+	 * @param sGraph graph
+	 * @param s source
+	 * @param t sink
+	 */
+	public PreflowPush(SimpleGraph sGraph) {
+        this.sGraph = sGraph;
+//        heights = new HeightList(graph.numVertices()+1);
+
+//        initialize();
+	}
+}
